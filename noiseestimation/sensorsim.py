00949c0d36dabc2e08d9048b897a634a29f734eb
Dominik Schulz <Kinodim@users.noreply.github.com>
2017-06-20 16:53:56 +0200

    Extend sensor simulator for arbitrary dimensions

--- noiseestimation/sensorsim.py
+++ noiseestimation/sensorsim.py
@@ -11,12 +11,14 @@ class SensorSim:
     no more data on its variance is available
     """
     def __init__(self, 
+            position,
+            velocity,
             measurement_std,
-            position = (0,0),
-            velocity = (0,0),
+            dim = 1,
             timestep = 0.1):
-        self.position = [ position[0], position[1] ] # needs to be mutable
-        self.velocity = velocity
+        self.dim = dim
+        self.position = np.asarray(position, dtype="float64").reshape(dim, 1)
+        self.velocity = np.asarray(velocity, dtype="float64").reshape(dim, 1)
         if not hasattr(measurement_std, '__iter__'):
             raise ValueError("The measurement noise std argument needs to be iterable")
         self.measurement_std = measurement_std
@@ -30,13 +32,9 @@ class SensorSim:
         if self.counter >= len(self.measurement_std):
             raise IndexError('No more measurement std data available')
 
-        self.position[0] += self.velocity[0]
-        self.position[1] += self.velocity[1]
-
-        measurement = np.asarray([ self.position[0] + randn() * self.measurement_std[self.counter],
-                 self.position[1] + randn() * self.measurement_std[self.counter] ]).reshape(2,1)
-        truth = np.asarray(self.position).reshape(2,1)
-        return measurement, truth
+        self.position += self.velocity
+        measurement = self.position + self.measurement_std[self.counter] * randn(self.dim, 1)
+        return measurement, self.position
 
 
     def batch_read(self):
--- tests/sensorsim_tests.py
+++ tests/sensorsim_tests.py
@@ -6,29 +6,44 @@ class TestSensorSim:
 
     def setup(self):
         self.measurement_std = [1, 2, 3]
-        self.sim = SensorSim(self.measurement_std)
+        self.sim1d = SensorSim( 0, 0.5, self.measurement_std, 1)
+        self.sim2d = SensorSim((0,0), (1, 2), self.measurement_std, 2)
 
     def basic_constructor_test(self):
         pass
 
     @raises(ValueError)
-    def constructor_argument_check_test(self):
-        sim = SensorSim(4)
+    def constructor_measurement_check_test(self):
+        sim = SensorSim(0, 1, 4, 1)
 
-    def read_all_test(self):
+    @raises(ValueError)
+    def constructor_position_check_test(self):
+        sim = SensorSim(0, (1, 1), [4], 2)
+    
+    @raises(ValueError)
+    def constructor_velocity_check_test(self):
+        sim = SensorSim((0, 0), 1, [4], 2)
+
+    def read_all_1d_test(self):
         for x in self.measurement_std:
-            meas, truth = self.sim.read()
+            meas, truth = self.sim1d.read()
+            assert meas.shape == (1,1)
+            assert truth.shape == (1,1)
+
+    def read_all_2d_test(self):
+        for x in self.measurement_std:
+            meas, truth = self.sim2d.read()
             assert meas.shape == (2,1)
             assert truth.shape == (2,1)
 
     @raises(IndexError)
     def read_too_many_test(self):
         for x in self.measurement_std:
-            self.sim.read()
-        self.sim.read()
+            self.sim2d.read()
+        self.sim2d.read()
 
-    def batch_read_test(self):
-        res = self.sim.batch_read()
+    def batch_read_2d_test(self):
+        res = self.sim2d.batch_read()
         assert len(res) == len(self.measurement_std)
         shape = np.asarray(res).shape
         assert shape[0] == len(self.measurement_std)
