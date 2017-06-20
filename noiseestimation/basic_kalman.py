790711d3ed1ab04e85c7d5c0f12e58f1510c0f54
Dominik Schulz <dominik.schulz@fake.com>
2017-06-20 11:39:38 +0200

    Plot estimation error

--- noiseestimation/basic_kalman.py
+++ noiseestimation/basic_kalman.py
@@ -5,12 +5,14 @@ from filterpy.common import Q_discrete_white_noise
 from matplotlib import pyplot as plt
 from sensorsim import SensorSim
 
+# set up sensor simulator
 dt = 0.1
 measurement_std_max = 4
-measurement_std = np.arange(0, measurement_std_max, .05)
+measurement_std = np.arange(0, measurement_std_max, .02)
 measurement_std = np.concatenate( (measurement_std, list(reversed(measurement_std)) ))
 sim = SensorSim(measurement_std, timestep=dt, velocity = (0.5, 1))
 
+# set up kalman filter
 tracker = KalmanFilter(dim_x=4, dim_z=2)
 tracker.F = np.array([[1, dt, 0,  0],
                       [0,  1, 0,  0],
@@ -24,12 +26,23 @@ tracker.R = np.diag([measurement_std_max, measurement_std_max])
 tracker.x = np.array([[0, 0, 0, 0]]).T
 tracker.P = np.eye(4) * 500
 
-readings = np.array([ np.array(sim.read()[0]).reshape(2,1) for _ in measurement_std ])
+# perform sensor simulation and filtering
+sensor_out = np.array([ np.array(sim.read()) for _ in measurement_std ])
+readings = sensor_out[:,0].reshape(-1,2,1)
+truth = sensor_out[:,1].reshape(-1,2,1)
 
 mu, cov, _, _ = tracker.batch_filter(readings)
+error = np.sqrt(np.sum(np.square(truth - mu[:,[0, 2]]), 1))
 
-plt.plot(readings[:,0], readings[:,1], 'go', label="Measurements")
-plt.plot(mu[:,0], mu[:,2], 'm', linewidth=3, label="Filter")
-plt.legend(loc="lower right")
-plt.title("Kalman filtering of position")
+# plot results
+f, axarr = plt.subplots(2)
+axarr[0].plot(readings[:,0], readings[:,1], 'go', label="Measurements")
+axarr[0].plot(mu[:,0], mu[:,2], 'm', linewidth=3, label="Filter")
+axarr[0].legend(loc="lower right")
+# axarr[0].set_xlim([0,200])
+axarr[0].set_title("Kalman filtering of position")
+
+axarr[1].plot(error, 'r')
+axarr[1].set_title("Estimation error")
+
 plt.show()
