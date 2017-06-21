86e815c40acaa276a499ce7c49a812c0bd14946a
Dominik Schulz <Kinodim@users.noreply.github.com>
2017-06-21 19:55:28 +0200

    Check whiteness in filter example

--- noiseestimation/mehra_linear.py
+++ noiseestimation/mehra_linear.py
@@ -4,21 +4,22 @@ from filterpy.kalman import KalmanFilter
 from filterpy.common import Q_discrete_white_noise
 from matplotlib import pyplot as plt
 from sensorsim import SensorSim
+from correlator import Correlator
 
 # set up sensor simulator
 dt = 0.1
-measurement_std = 2
-measurement_std_list = np.asarray([measurement_std] * 1000)
+measurement_std = 2.
+measurement_std_list = np.asarray([measurement_std] * 2000)
 sim = SensorSim(0, 0.1, measurement_std_list, 1, timestep=dt)
 
 # set up kalman filter
 tracker = KalmanFilter(dim_x=2, dim_z=1)
 tracker.F = np.array([[1, dt],
                       [0,  1]])
-q = Q_discrete_white_noise(dim=2, dt=dt, var=0.001)
+q = 0#Q_discrete_white_noise(dim=2, dt=dt, var=0.001)
 tracker.Q = q
 tracker.H = np.array([[1, 0]])
-tracker.R = measurement_std
+tracker.R = measurement_std**2
 tracker.x = np.array([[0, 0]]).T
 tracker.P = np.eye(2) * 500
 
@@ -39,18 +40,21 @@ for _ in measurement_std_list:
 error = np.asarray(truths) - mu
 
 # plot results
-f, axarr = plt.subplots(3,sharex=True)
+f, axarr = plt.subplots(3,sharex=False)
 axarr[0].plot(readings, 'go', label="Measurements")
 axarr[0].plot(mu, 'm', linewidth=3, label="Filter")
 axarr[0].legend(loc="lower right")
 # axarr[0].set_xlim([0,200])
 axarr[0].set_title("Kalman filtering of position")
 
-axarr[1].plot(error, 'r')
-axarr[1].set_title("Estimation error")
+# axarr[1].plot(error, 'r')
+# axarr[1].set_title("Estimation error")
 
 axarr[2].plot(residuals, 'b')
 axarr[2].set_title("Residuals")
 
-plt.xlabel("Sample")
+cor = Correlator(residuals[100:], 100)
+axarr[1].plot(cor.autocorrelation()[1:],'ro')
+print cor.isWhite()
+
 plt.show()
