de92c155a7840ede1a13032a9b8b16a1aae5b376
Dominik Schulz <Kinodim@users.noreply.github.com>
2017-06-24 00:56:58 +0200

    Use noise estimation in mehra example

--- noiseestimation/mehra_linear.py
+++ noiseestimation/mehra_linear.py
@@ -5,21 +5,22 @@ from filterpy.common import Q_discrete_white_noise
 from matplotlib import pyplot as plt
 from sensorsim import SensorSim
 from correlator import Correlator
+from noiseestimator import NoiseEstimator
 
 # set up sensor simulator
 dt = 0.1
-measurement_std = 2.
-measurement_std_list = np.asarray([measurement_std] * 2000)
+measurement_std = 5.
+measurement_std_list = np.asarray([measurement_std] * 200)
 sim = SensorSim(0, 0.1, measurement_std_list, 1, timestep=dt)
 
 # set up kalman filter
 tracker = KalmanFilter(dim_x=2, dim_z=1)
 tracker.F = np.array([[1, dt],
                       [0,  1]])
-q = 0#Q_discrete_white_noise(dim=2, dt=dt, var=0.001)
+q = Q_discrete_white_noise(dim=2, dt=dt, var=0.001)
 tracker.Q = q
 tracker.H = np.array([[1, 0]])
-tracker.R = measurement_std**2
+tracker.R = measurement_std**2 / 20
 tracker.x = np.array([[0, 0]]).T
 tracker.P = np.eye(2) * 500
 
@@ -40,21 +41,33 @@ for _ in measurement_std_list:
 error = np.asarray(truths) - mu
 
 # plot results
-f, axarr = plt.subplots(3,sharex=False)
+f, axarr = plt.subplots(3,sharex=True)
 axarr[0].plot(readings, 'go', label="Measurements")
 axarr[0].plot(mu, 'm', linewidth=3, label="Filter")
 axarr[0].legend(loc="lower right")
 # axarr[0].set_xlim([0,200])
 axarr[0].set_title("Kalman filtering of position")
 
-# axarr[1].plot(error, 'r')
-# axarr[1].set_title("Estimation error")
+axarr[1].plot(error, 'r')
+axarr[1].set_title("Estimation error")
 
 axarr[2].plot(residuals, 'b')
 axarr[2].set_title("Residuals")
 
-cor = Correlator(residuals[100:], 100)
-axarr[1].plot(cor.autocorrelation()[1:],'ro')
+cor = Correlator(residuals[-50:])
+print cor.isWhite()
+cor = Correlator(residuals)
 print cor.isWhite()
 
+cor = Correlator(residuals[-50:])
+
 plt.show()
+
+R = NoiseEstimator.estimate(cor.covariance(100), tracker.K, tracker.F, tracker.H)
+abs_err = measurement_std**2 - R
+rel_err = abs_err / measurement_std**2
+print "True: %.3f" % measurement_std**2
+print "Filter: %.3f" % tracker.R
+print "Estimated: %.3f" % R
+print "Absolute error: %.3f" % abs_err
+print "Relative error: %.3f %%" % (rel_err * 100)
--- noiseestimation/noiseestimator.py
+++ noiseestimation/noiseestimator.py
@@ -1,10 +1,9 @@
 import numpy as np
 from numpy.linalg import pinv
 
-class NoiseEstimator:
+class NoiseEstimator(object):
     @staticmethod
     def estimate(C_arr, K, F, H):
-
         C_arr = np.asarray(C_arr).reshape( (-1, H.shape[0]) )
         N = len(C_arr)
         # construct matrix A
@@ -23,4 +22,4 @@ class NoiseEstimator:
         A = A.reshape( (-1, F.shape[1]))
         MH = np.dot(K, C_arr[0,:,None]) + np.dot( pinv(A), C_arr)
         R = C_arr[0] - np.dot(H, MH)
-        print R
+        return R
