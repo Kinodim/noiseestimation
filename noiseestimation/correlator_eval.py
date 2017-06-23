ec4dbfffc083326fe1a91d6a80aaec456d2b055e
Dominik Schulz <Kinodim@users.noreply.github.com>
2017-06-23 16:32:55 +0200

    Add correlator evaluation

---
+++ noiseestimation/correlator_eval.py
@@ -0,0 +1,20 @@
+from numpy.random import randn
+from correlator import Correlator
+
+num_runs = 50
+arr_length = 1000
+
+num_mehra_white = 0
+num_ljungbox_white = 0
+for i in range(num_runs):
+    seq = randn(arr_length)
+    cor = Correlator(seq)
+
+    if cor.isWhite() == True:
+        num_ljungbox_white += 1
+    if cor.isWhite('mehra') == True:
+        num_mehra_white += 1
+
+print "-" * 10
+print "%d / %d white (%.3f %%) using Mehra method" % (num_mehra_white, num_runs, 100 * float(num_mehra_white)/ num_runs)
+print "%d / %d white (%.3f %%) using Ljund-Box method" % (num_ljungbox_white, num_runs, 100 * float(num_ljungbox_white)/ num_runs)
