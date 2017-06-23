307105e88f4d332c24f3b15009a1d3c3136ff3c2
Dominik Schulz <Kinodim@users.noreply.github.com>
2017-06-23 16:32:10 +0200

    Change Correlator to also offer Ljung-Box test and use it as default

--- noiseestimation/correlator.py
+++ noiseestimation/correlator.py
@@ -1,22 +1,28 @@
-from math import sqrt
+from math import sqrt, ceil
+from statsmodels.stats.diagnostic import acorr_ljungbox
 import numpy as np
 
 class Correlator:
     """
     Provides functionality related to the autocorrelation coefficients
-    of a sequence of values. More specifically, it calculates whether
-    this sequence is some white noise using its estimated autocorrelation
-    and the times they exceed the 95% confidence interval.
-    Adapted from: On the Identification of Variances and Adaptive
-    Kalman Filtering, Mehra 1970
-    """
-    def __init__(self, values, max_lag):
-        self.values = values
-        self.max_lag = max_lag
+    of a sequence of values, more specifically to check whether the
+    sequence is some form of white noise.
 
-    def covariance(self):
+    Two options to check this are offered:
+    The first is to use its estimated autocorrelation
+    and the times they exceed the 95% confidence interval.
+    This approach is adapted from: On the Identification of Variances
+    and Adaptive Kalman Filtering, Mehra 1970
+
+    The alternative is to employ the Ljung-Box test implemented in the
+    statsmodel package
+    """
+    def __init__(self, values):
+        self.values = values
+
+    def covariance(self, lags):
         C = []
-        for k in range(self.max_lag + 1):
+        for k in range(lags + 1):
             c = .0
             for i in range(k, len(self.values)):
                 c += self.values[i] * self.values[i-k]
@@ -24,22 +30,30 @@ class Correlator:
             C.append(c)
         return C
 
-    def autocorrelation(self):
-        C = self.covariance()
+    def autocorrelation(self, lags):
+        C = self.covariance(lags)
         rho = [c / C[0] for c in C]
         return rho
 
-    def isWhite(self):
-        limit = 1.96 / sqrt(len(self.values))
-        outliers = 0
-        rho = self.autocorrelation()
-        for elem in rho[1:]:
-            if -limit <= elem <= limit:
-                continue
-            else:
-                outliers += 1
+    def isWhite(self, method = 'ljung-box', lags = 0):
+        if method == 'mehra':
+            limit = 1.96 / sqrt(len(self.values))
+            outliers = 0
+            if lags == 0:
+                lags = int(ceil(len(self.values) / 10.0))
+            rho = self.autocorrelation(lags)
+            for elem in rho[1:]:
+                if -limit <= elem <= limit:
+                    continue
+                else:
+                    outliers += 1
 
-        print float(outliers) / (len(rho) - 1)
-        if float(outliers) / (len(rho) - 1) >= 0.05:
-            return False
-        return True
+            if float(outliers) / (len(rho) - 1) >= 0.05:
+                return False
+            return True
+        elif method == 'ljung-box':
+            # use 10 lags as proposed by R. Hyndman
+            pval = acorr_ljungbox(self.values, 10)[1][-1] # last entry from p-values array
+            return pval >= 0.05
+        else:
+            raise ValueError("Method %s is not a valid argument" % method)
--- tests/correlator_test.py
+++ tests/correlator_test.py
@@ -1,33 +1,37 @@
 import numpy as np
+import pytest
 from noiseestimation.correlator import *
 
 class TestCorrelator:
     def setup(self):
         arr = [1, 2, 1]
-        self.cor = Correlator(arr, 2)
+        self.cor = Correlator(arr)
 
     def test_basic_constructor(self):
         pass
 
     def test_covariance(self):
-        C = self.cor.covariance()
+        C = self.cor.covariance(2)
         assert len(C) == 3
         assert C[0] == (1 + 4 + 1) / 3.
         assert C[1] == (2 + 2) / 3.
         assert C[2] == 1 / 3.
 
     def test_autocorrelation(self):
-        rho = self.cor.autocorrelation()
+        rho = self.cor.autocorrelation(2)
         assert len(rho) == 3
         assert rho[0] == 1
         assert rho[1] == 2. / 3
         assert rho[2] == 1. / 6
 
     def test_isWhite(self):
-        notwhite = Correlator([5]*100, 50)
-        assert notwhite.isWhite() == False
-        white = Correlator([0,0,1,0,0], 4)
-        assert white.isWhite() == True
-        
-        # arr = [1, 1, 2, 2, 3, 3, -1, -2, -5, 10]
-        # self.cor = Correlator(arr, 5)
+        notwhite = Correlator([.1]*100)
+        assert notwhite.isWhite('mehra') == False
+        assert notwhite.isWhite('ljung-box') == False
+
+        white = Correlator([0]*50 + [1] + [0]*50)
+        assert white.isWhite('mehra') == True
+        assert white.isWhite('ljung-box') == True
+
+        with pytest.raises(ValueError):
+            white.isWhite('novalidmethod')
