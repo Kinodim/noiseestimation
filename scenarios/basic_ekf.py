0a1d49cde80749b214a85efabb413a38122c3810
Dominik Schulz <Kinodim@users.noreply.github.com>
2017-07-18 16:02:46 +0200

    Complete basic extended kalman filter

--- scenarios/basic_ekf.py
+++ scenarios/basic_ekf.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 from scipy.linalg import block_diag
 from filterpy.kalman import ExtendedKalmanFilter
 from filterpy.common import Q_discrete_white_noise
@@ -6,20 +7,37 @@ from matplotlib import pyplot as plt
 from noiseestimation.sensor import Sensor
 
 # parameters
-measurement_var_max = 0.001
-num_samples = 600
+measurement_var_max = 0.01
+num_samples = 300
 dt = 0.1
 
 
-# return range measurement
+# return range and bearing measurement
 def h(x):
-    return (x[0, 0]**2 + x[2, 0] ** 2) ** 0.5
+    distance = (x[0, 0]**2 + x[2, 0] ** 2) ** 0.5 
+    bearing = math.atan2(x[2, 0], x[0, 0])
 
+    return np.array([[distance],
+                     [bearing]])
 
 def H_jacobian_at(x):
-    denom = (x[0, 0]**2 + x[2, 0]**2) ** 0.5
-    return np.array([[x[0, 0] / denom, 0., x[2, 0] / denom]])
+    hyp = x[0, 0]**2 + x[2, 0]**2
+    dist = hyp ** 0.5
+    return np.array(
+        [[x[0, 0] / dist, 0., x[2, 0] / dist],
+         [-x[2, 0] / hyp, 0., x[0, 0] / hyp]])
 
+def normalize_angle(x):
+    x = x % (2 * np.pi)
+    if x > np.pi:
+        x -= 2 * np.pi
+    return x
+
+def custom_residual(a, b):
+    res = np.array(
+            [[a[0, 0] - b[0, 0]],
+             [normalize_angle(a[1, 0] - b[1, 0])]])
+    return res
 
 def setup():
     # set up sensor simulator
@@ -30,20 +48,20 @@ def setup():
     def f(x):
         return np.dot(F, x)
 
-    x0 = np.array([[-10],
-                   [0.4],
-                   [5]])
+    x0 = np.array([[-3],
+                   [0.5],
+                   [1]])
     sim = Sensor(x0, f, h)
 
     # set up kalman filter
-    tracker = ExtendedKalmanFilter(dim_x=3, dim_z=1)
+    tracker = ExtendedKalmanFilter(dim_x=3, dim_z=2)
     tracker.F = F
     q_x = Q_discrete_white_noise(dim=2, dt=dt, var=0.001)
     q_y = 0.001 * dt**2
     tracker.Q = block_diag(q_x, q_y)
     tracker.R = measurement_var_max
-    tracker.x = np.array([[-10.1, 0.4, 5.1]]).T
-    tracker.P = np.eye(3) * 1
+    tracker.x = np.array([[2, -0.1, 4]]).T
+    tracker.P = np.eye(3) * 500
 
     return sim, tracker
 
@@ -54,7 +72,9 @@ def filtering(sim, tracker):
     measurement_vars = np.concatenate(
         (measurement_vars, list(reversed(measurement_vars))))
     Rs = [np.eye(2) * measurement_var for measurement_var in measurement_vars]
-    Rs = [[[measurement_var_max]]] * num_samples
+    R_proto = np.array([[1, 0],
+                        [0, 0.01]])
+    Rs = [R_proto * measurement_var_max] * num_samples
     readings = []
     truths = []
     filtered = []
@@ -62,7 +82,7 @@ def filtering(sim, tracker):
         sim.step()
         reading = sim.read(R)
         tracker.predict()
-        tracker.update(reading, H_jacobian_at, h)
+        tracker.update(reading, H_jacobian_at, h, residual = custom_residual)
         readings.append(reading)
         truths.append(sim.x)
         filtered.append(tracker.x)
@@ -77,12 +97,16 @@ def filtering(sim, tracker):
     return readings, truths, filtered, error
 
 
-def plot_results(readings, filtered, error):
-    f, axarr = plt.subplots(3)
+def plot_results(readings, filtered, truths):
+    f, axarr = plt.subplots(4)
     axarr[0].plot(
         filtered[:, 0],
         filtered[:, 2],
         'm', linewidth=3, label="Filter")
+    axarr[0].plot(
+        truths[:, 0],
+        truths[:, 2],
+        'k', linewidth=3, label="Truth")
     axarr[0].legend(loc="lower right")
     axarr[0].set_title("Kalman filtering of position")
 
@@ -95,6 +119,9 @@ def plot_results(readings, filtered, error):
     axarr[2].plot(readings[:, 0, 0], 'go', label="Measurements")
     axarr[2].set_title("Range measurements")
 
+    axarr[3].plot(readings[:, 1, 0] * 180 / math.pi, 'go', label="Measurements")
+    axarr[3].set_title("Bearing measurements")
+
     # axarr[1].plot(error, 'r')
     # axarr[1].set_title("Estimation error")
 
@@ -104,7 +131,7 @@ def plot_results(readings, filtered, error):
 def run_tracker():
     sim, tracker = setup()
     readings, truths, filtered, error = filtering(sim, tracker)
-    plot_results(readings, filtered, error)
+    plot_results(readings, filtered, truths)
 
 
 if __name__ == "__main__":
