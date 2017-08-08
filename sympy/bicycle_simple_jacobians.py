40fa9dd6a6d8c95fdb02a3c003a8a8f136f49ee7
Dominik Schulz <Kinodim@users.noreply.github.com>
2017-08-08 19:19:08 +0200

    Show jacobians for alpha=0

--- sympy/bicycle_simple_jacobians.py
+++ sympy/bicycle_simple_jacobians.py
@@ -1,5 +1,5 @@
 import sympy
-from sympy.abc import alpha, x, y, v, w, R, theta
+from sympy.abc import alpha, x, y, v, w, theta
 from sympy import symbols, Matrix, pprint
 
 
@@ -10,17 +10,21 @@ if __name__ == "__main__":
     r = w/sympy.tan(alpha)
 
     fxu = Matrix([[x - r * sympy.sin(theta) + r * sympy.sin(theta + beta)],
-    [y + r * sympy.cos(theta) - r * sympy.cos(theta + beta)],
-    [theta + beta]])
+                  [y + r * sympy.cos(theta) - r * sympy.cos(theta + beta)],
+                  [theta + beta]])
     F = fxu.jacobian(Matrix([x, y, theta]))
 
     B, R = symbols('beta, R')
     F = F.subs((d / w) * sympy.tan(alpha), B)
-    F = F.subs(w /sympy.tan(alpha), R)
+    F = F.subs(w / sympy.tan(alpha), R)
     print("\nJacobian of F with respect to state:")
     pprint(F)
-    print("Raw:")
+    print("\nRaw:")
     print(F)
+    print("\nWith alpha = 0:")
+    F = F.subs(B, 0)
+    F = F.subs(R, sympy.oo)
+    pprint(F)
 
     V = fxu.jacobian(Matrix([v, alpha]))
     V = V.subs(sympy.tan(alpha) / w, 1/R)
@@ -29,5 +33,10 @@ if __name__ == "__main__":
     V = V.subs(time * v, 'd')
     print("\nJacobian of F with respect to control:")
     pprint(V)
-    print("Raw:")
+    print("\nRaw:")
     print(V)
+    print("\nWith alpha = 0:")
+    V = V.subs(B, 0)
+    V = V.subs(R, sympy.oo)
+    V = V.subs(alpha, 0)
+    pprint(V)
