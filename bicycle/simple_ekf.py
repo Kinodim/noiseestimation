edb35dc67da798a81f1f44d1d5f2b9c80d06cac0
Dominik Schulz <Kinodim@users.noreply.github.com>
2017-08-08 19:19:28 +0200

    Add adapted ekf class

---
+++ bicycle/simple_ekf.py
@@ -0,0 +1,77 @@
+from filterpy.kalman import ExtendedKalmanFilter as EKF
+import numpy as np
+import math
+from math import sin, cos, tan, sqrt
+
+
+class SimpleBikeEKF(EKF):
+    turning_threshold_angle = 0.001
+
+    def __init__(self, dt, wheelbase, var_vel, var_steer):
+        EKF.__init__(self, 3, 2, 2)
+        self.dt = dt
+        self.wheelbase = wheelbase
+        self.var_vel = var_vel
+        self.var_steer = var_steer
+
+    def predict(self, u=0):
+        self.move(u)
+        F = self.Fx(u)
+        B = self.Bx(u)
+
+        # covariance of motion in control space
+        M = np.diag(self.var_vel, self.var_steer)
+        self.P = np.dot(F, np.dot(self.P, F.T)) + np.dot(B, np.dot(M, B.T))
+
+    def move(self, u):
+        heading = self.x[2, 0]
+        vel = u[0, 0]
+        steering_angle = u[1, 0]
+        dist = vel * self.dt
+
+        # check whether robot is turning
+        if abs(steering_angle) > self.turning_threshold_angle:
+            beta = (dist / self.wheelbase) * tan(steering_angle)
+            r = self.wheelbase / tan(steering_angle)
+            dx = np.array([[-r * sin(heading) + r * sin(heading + beta)],
+                           [r * cos(heading) - r * cos(heading + beta)],
+                           [beta]])
+        else:
+            dx = np.array([[dist * cos(heading)],
+                           [dist * sin(heading)],
+                           [0]])
+
+        self.x += dx
+
+    def Fx(self, u):
+        vel = u[1, 0]
+        steering_angle = u[1, 0]
+        theta = self.x[2, 0]
+        if abs(steering_angle) < self.turning_threshold_angle:
+            return np.eye(3)
+
+        beta = (vel * self.dt / self.wheelbase) * tan(steering_angle)
+        R = self.wheelbase / tan(steering_angle)
+        return np.array(
+            [[1, 0, -R*cos(theta) + R*cos(beta + theta)],
+             [0, 1, -R*sin(theta) + R*sin(beta + theta)],
+             [0, 0, 1]])
+
+    def Bx(self, u):
+        vel = u[1, 0]
+        steering_angle = u[1, 0]
+        theta = self.x[2, 0]
+        if abs(steering_angle) < self.turning_threshold_angle:
+            return np.eye(3)
+
+        d = vel * self.dt
+        beta = (d / self.wheelbase) * tan(steering_angle)
+        R = self.wheelbase / tan(steering_angle)
+        alpha = steering_angle
+        return np.array(
+            [[self.dt*cos(beta + theta),
+              -R*(-tan(alpha)**2 - 1)*sin(theta)/tan(alpha) + R*(-tan(alpha)**2 - 1)*sin(beta + theta)/tan(alpha) + d*(tan(alpha)**2 + 1)*cos(beta + theta)/tan(alpha)],
+             [self.dt*sin(beta + theta),
+              R*(-tan(alpha)**2 - 1)*cos(theta)/tan(alpha) - R*(-tan(alpha)**2 - 1)*cos(beta + theta)/tan(alpha) + d*(tan(alpha)**2 + 1)*sin(beta + theta)/tan(alpha)],
+             [self.dt/R,
+              d*(tan(alpha)**2 + 1)/self.wheelbase]])
