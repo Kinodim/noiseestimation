94964e02e7999680cbf10539227c7f22cfb99eda
Dominik Schulz <Kinodim@users.noreply.github.com>
2017-08-23 14:42:23 +0200

    Refactor extended estimation

--- noiseestimation/estimation.py
+++ noiseestimation/estimation.py
@@ -3,25 +3,7 @@ from numpy.linalg import pinv
 
 
 def estimate_noise(C_arr, K, F, H, return_state_covariance=False):
-    """Estimates noise based on the innovation correlation
-
-    This function implements the approach proposed by Mehra.
-    Using the matrices of a Kalman Filter and the innovation
-    correlation, it calculates an estimation of the
-    actual measurement noise covariance.
-    For more information please refer to:
-    On the Identification of Variances and Adaptive Kalman
-    Filtering, Mehra 1970
-
-    Args:
-        C_arr (ndarray): The list of innovation correlation estimates
-        K (ndarray): Kalman gain
-        F (ndarray): Update matrix
-        H (ndarray): Measurement matrix
-
-    Returns:
-        ndarray: The estimated measurement noise covariance matrix
-
+    """An experimental variation of the mehra method
     """
     N = len(C_arr)
     num_observations = H.shape[0]
@@ -50,7 +32,24 @@ def estimate_noise(C_arr, K, F, H, return_state_covariance=False):
 
 
 def estimate_noise_mehra(C_arr, K, F, H):
-    """estimate using mehra version
+    """Estimates noise based on the innovation correlation
+
+    This function implements the approach proposed by Mehra.
+    Using the matrices of a Kalman Filter and the innovation
+    correlation, it calculates an estimation of the
+    actual measurement noise covariance.
+    For more information please refer to:
+    On the Identification of Variances and Adaptive Kalman
+    Filtering, Mehra 1970
+
+    Args:
+        C_arr (ndarray): The list of innovation correlation estimates
+        K (ndarray): Kalman gain
+        F (ndarray): Update matrix
+        H (ndarray): Measurement matrix
+
+    Returns:
+        ndarray: The estimated measurement noise covariance matrix
     """
     N = len(C_arr)
     num_observations = H.shape[0]
@@ -110,70 +109,55 @@ def estimate_noise_approx(G, H, P, residual_type="prior"):
     return R
 
 
-def estimate_noise_extended(C_arr, K, F_arr, H_arr):
-    """estimate using adapted version for EKF
+def estimate_noise_extended(C_arr, K, F, H):
+    """Estimates using adapted version for EKF with time-varying matrices
+
     Args:
         C_arr (ndarray): The list of innovation correlation estimates
         K (ndarray): Kalman gain
-        F_arr (ndarray): Update matrix list (starting with most recent)
-        H_arr (ndarray): Measurement matrix list
+        F (ndarray): Update matrix
+        H (ndarray): Measurement matrix
+
+    Kalman gain, update matrix and the measurement matrix can be lists of those
+    entities, containing the respective matrix for the according timestep
+    starting with the oldest entry first
 
     Returns:
         ndarray: The estimated measurement noise covariance matrix
     """
     N = len(C_arr)
 
-    if H_arr.ndim == 3:
-        A = __construct_A_nonlinear_H(N, K, F_arr, H_arr)
-        num_observations = H_arr[0].shape[0]
-    elif F_arr.ndim == 3:
-        A = __construct_A_nonlinear_F(N, K, F_arr, H_arr)
-        num_observations = H_arr.shape[0]
+    # most recent entry first
+    K = __reverse_or_create_list(K, N)
+    F = __reverse_or_create_list(F, N)
+    H = __reverse_or_create_list(H, N)
+
+    num_observations = H[0].shape[0]
+    num_states = F[0].shape[0]
+    A = np.ndarray((0, num_observations, num_states))
+    # product = Product{F[n](I-K[n]H[n])}
+    product = np.eye(num_states)
+    for n in range(N - 1):
+        if n != 0:
+            # F[n] * (I - K[n]*H[n])
+            bracket = np.dot(F[n], np.eye(num_states) - np.dot(K[n], H[n]))
+            # watch out for order of multiplication: [0]...[n]
+            product = np.dot(product, bracket)
+        entry = np.dot(H[0], product)
+        entry = np.dot(entry, F[0])
+        A = np.vstack((A, [entry]))
+
+    A = A.reshape((-1, num_states))
 
     C_stacked = C_arr[1:].reshape((-1, num_observations))
-    MH = np.dot(K, C_arr[0]) + np.dot(pinv(A), C_stacked)
-    H = H_arr[0] if H_arr.ndim == 3 else H_arr
-    R = C_arr[0] - np.dot(H, MH)
+    MH = np.dot(K[0], C_arr[0]) + np.dot(pinv(A), C_stacked)
+    R = C_arr[0] - np.dot(H[0], MH)
     return R
 
 
-def __construct_A_nonlinear_H(N, K, F, H_arr):
-    num_observations = H_arr[0].shape[0]
-    num_states = F.shape[0]
-
-    A = np.ndarray((0, num_observations, num_states))
-    # product = Product{F(I-KH[n])}
-    product = np.eye(num_states)
-    for n in range(N - 1):
-        if n != 0:
-            # F * (I - K*H[n])
-            bracket = np.dot(F, np.eye(num_states) - np.dot(K, H_arr[n]))
-            # watch out for order of multiplication: H[0]...H[n]
-            product = np.dot(product, bracket)
-        entry = np.dot(H_arr[0], product)
-        entry = np.dot(entry, F)
-        A = np.vstack((A, [entry]))
-
-    A = A.reshape((-1, num_states))
-    return A
-
-
-def __construct_A_nonlinear_F(N, K, F_arr, H):
-    num_observations = H.shape[0]
-    num_states = F_arr[0].shape[0]
-
-    A = np.ndarray((0, num_observations, num_states))
-    # product = Product{F[n](I-KH)}
-    product = np.eye(num_states)
-    for n in range(N - 1):
-        if n != 0:
-            # F[n] * (I - K*H)
-            bracket = np.dot(F_arr[n], np.eye(num_states) - np.dot(K, H))
-            # watch out for order of multiplication: F[0]...F[n]
-            product = np.dot(product, bracket)
-        entry = np.dot(H, product)
-        entry = np.dot(entry, F_arr[0])
-        A = np.vstack((A, [entry]))
-
-    A = A.reshape((-1, num_states))
-    return A
+def __reverse_or_create_list(x, N):
+    if x.ndim == 3:
+        x = x[::-1]
+    else:
+        x = [x] * (N - 1)
+    return x
