409c5b0ded22b27e06768aa65a92a95a803e0ae8
Dominik Schulz <Kinodim@users.noreply.github.com>
2017-10-07 22:01:23 +0200

    Adapt linear 2d for presentation

--- linear/estimate_2d.py
+++ linear/estimate_2d.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 import numpy as np
-import math
+import json
 from scipy.linalg import block_diag
 from filterpy.kalman import KalmanFilter
 from filterpy.common import Q_discrete_white_noise
@@ -8,20 +8,21 @@ from matplotlib import pyplot as plt
 from noiseestimation.sensor import LinearSensor
 from noiseestimation.correlator import Correlator
 from noiseestimation.estimation import (
-    estimate_noise,
     estimate_noise_approx,
-    # estimate_noise_mehra
+    estimate_noise_mehra
 )
 
 
 # parameters
-runs = 20
+runs = 1
 sample_size = 200
 used_taps = 100
-measurement_var = 5
-R_proto = np.array([[1, 0.4],
-                    [0.4, 0.8]])
-Rs = [R_proto * measurement_var] * sample_size
+skip_samples = 50
+measurement_var = 1
+R_proto = np.array([[1, 0.1],
+                    [0.1, 0.6]])
+Rs = [R_proto * measurement_var] * (sample_size + skip_samples)
+filter_misestimation_factor = 1.0
 
 
 def setup():
@@ -41,10 +42,13 @@ def setup():
     # set up kalman filter
     tracker = KalmanFilter(dim_x=4, dim_z=2)
     tracker.F = F
-    q = Q_discrete_white_noise(dim=2, dt=dt, var=0.01)
+    q = Q_discrete_white_noise(dim=2, dt=dt, var=1e-3)
     tracker.Q = block_diag(q, q)
     tracker.H = H
-    tracker.R = np.diag([measurement_var, measurement_var])
+    tracker.R = np.diag([measurement_var, measurement_var]) * \
+        filter_misestimation_factor
+    # tracker.R = np.array([[ 0.95641957,  0.22179505],
+    #                       [ 0.19764957,  0.69235086]])
     tracker.x = np.array([[0, 0, 0, 0]]).T
     tracker.P = np.eye(4) * 500
     return sim, tracker
@@ -73,17 +77,18 @@ def filtering(sim, tracker):
     return readings, truths, filtered, residuals
 
 
-def plot_results(readings, mu, error):
-    # plot results
-    f, axarr = plt.subplots(2)
-    axarr[0].plot(readings[:, 0], readings[:, 1], 'go', label="Measurements")
-    axarr[0].plot(mu[:, 0], mu[:, 1], 'm', linewidth=3, label="Filter")
-    axarr[0].legend(loc="lower right")
-    # axarr[0].set_xlim([0,200])
-    axarr[0].set_title("Kalman filtering of position")
-
-    axarr[1].plot(error, 'r')
-    axarr[1].set_title("Estimation error")
+def plot_results(readings, filtered):
+    plt.plot(readings[:, 0, 0],
+             readings[:, 1, 0],
+             'go', label="Measurements")
+    plt.plot(filtered[:, 0, 0],
+             filtered[:, 2, 0],
+             'm', linewidth=3, label="Filter")
+    plt.legend(loc="lower right")
+    # plt.xlim([0, 100])
+    plt.title("Kalman filtering of position")
+    plt.xlabel("x (m)")
+    plt.ylabel("y (m)")
 
     plt.show()
 
@@ -93,29 +98,22 @@ def matrix_error(estimate, truth):
 
 
 def perform_estimation(residuals, tracker, lags):
-    cor = Correlator(residuals)
+    cor = Correlator(residuals[-sample_size:])
     correlation = cor.autocorrelation(lags)
-    R = estimate_noise(
+    R_mehra = estimate_noise_mehra(
         correlation, tracker.K, tracker.F, tracker.H)
     R_approx = estimate_noise_approx(
         correlation[0], tracker.H, tracker.P, "posterior")
     truth = R_proto * measurement_var
-    error = matrix_error(R, truth)
+    error_mehra = matrix_error(R_mehra, truth)
+    error_approx = matrix_error(R_approx, truth)
     print("Truth:\n", truth)
-    print("Estimation:\n", R)
-    print("Error:\n", matrix_error(R, truth))
-    print("Approximated estimation:\n", R_approx)
-    print("Error:\n", matrix_error(R_approx, truth))
+    print("Estimation Mehra:\n", R_mehra)
+    print("Error Mehra:\n", error_mehra)
+    print("Estimation Mohamed:\n", R_approx)
+    print("Error Mohamed:\n", error_approx)
     print("-" * 15)
-    return error
-    # abs_err = measurement_std**2 - R
-    # rel_err = abs_err / measurement_std**2
-    # print "True: %.3f" % measurement_std**2
-    # print "Filter: %.3f" % tracker.R
-    # print "Estimated: %.3f" % R
-    # print "Absolute error: %.3f" % abs_err
-    # print "Relative error: %.3f %%" % (rel_err * 100)
-    # return rel_err
+    return (error_mehra, error_approx)
 
 
 def run_tracker():
@@ -123,17 +121,28 @@ def run_tracker():
     sim, tracker = setup()
     readings, truths, filtered, residuals = filtering(sim, tracker)
 
-    # plot_results(readings, mu, error)
+    plot_results(readings, filtered)
 
     error = perform_estimation(residuals, tracker, used_taps)
     return error
 
 
 if __name__ == "__main__":
-    run_tracker()
-    sum = .0
+    sum = np.zeros(2)
     for i in range(runs):
         print("%d / %d" % (i+1, runs))
-        sum += math.fabs(run_tracker())
+        sum += np.abs(run_tracker())
 
-    print("Avg absolute error: %.3f" % (sum / runs))
+    truth = R_proto * measurement_var
+    matrix_size = np.sqrt(np.sum(np.square(truth)))
+    print("Avg Mehra: %.6f" % (sum[0] / runs))
+    print("Avg Mohamed: %.6f" % (sum[1] / runs))
+
+    results = {}
+    results["Mehra absolute"] = sum[0] / runs
+    results["Mehra relative"] = sum[0] / matrix_size / runs
+    results["Mohamed absolute"] = sum[1] / runs
+    results["Mohamed relative"] = sum[1] / matrix_size / runs
+
+    with open("results.json", "w") as outfile:
+        json.dump(results, outfile)
