e37a07265a15ee2c77637edef9637c2129a8fbc6
Dominik Schulz <Kinodim@users.noreply.github.com>
2017-10-19 18:43:38 +0200

    Add ukf bicycle comparison

--- ukf/bicycle/estimate_continuous.py
+++ ukf/bicycle/estimate_continuous.py
@@ -6,15 +6,15 @@ from noiseestimation.playback_sensor import PlaybackSensor
 from bicycle_ukf import BicycleUKF
 from noiseestimation.correlator import Correlator
 from noiseestimation.estimation import (
-    estimate_noise_ukf_ml
+    estimate_noise_ukf_ml,
+    estimate_noise_ukf_scaling
 )
 
 # parameters
-skip_samples = 80
-window_size = 100
-num_windows = 12
+skip_samples = 300
+window_size = 150
+num_windows = 10
 used_taps = window_size / 2
-average_coefficient = 0.3
 
 measurement_var = 1e-5
 R_proto = np.array([[1, 0],
@@ -33,8 +33,8 @@ def setup():
     # set up kalman filter
     tracker = BicycleUKF(dt)
     Q_factor = np.array(5e-7)
-    tracker.Q = np.diag([1, 1, 1] * Q_factor)
-    tracker.R = R_proto * (sim_var + measurement_var) * filter_misestimation_factor
+    tracker.Q = np.diag([1, 1, 0.5] * Q_factor)
+    tracker.R = R_proto * (sim_var) * filter_misestimation_factor
     tracker.x = np.array([0, 0, 1]).T
     tracker.P = np.eye(3) * 1e0
 
@@ -97,8 +97,9 @@ def perform_estimation(residuals, tracker):
     residuals = residuals - np.average(residuals, axis=0)
     cor = Correlator(residuals)
     C_arr = cor.autocorrelation(used_taps)
-    R_ml = estimate_noise_ukf_ml(C_arr[0], tracker.Pz)
-    return R_ml
+    # R_ml = estimate_noise_ukf_ml(C_arr[0], tracker.Pz)
+    R_scaled = estimate_noise_ukf_scaling(C_arr[0], tracker.Pz, tracker.R)
+    return R_scaled
 
 
 def plot_results(readings, filtered, Ps, meas_dts):
@@ -163,16 +164,20 @@ def plot_position(readings, filtered, dts):
 
 
 def plot_noise_matrices(Rs, Rs_estimated, truth):
-    f, axarr = plt.subplots(2)
-    axarr[0].set_title("R[0, 0]")
-    axarr[0].plot(Rs[:, 0, 0])
-    axarr[0].plot([truth[0, 0]] * len(Rs))
-    axarr[0].plot(Rs_estimated[:, 0, 0], 'o')
+    f, axarr = plt.subplots(2, sharex=True)
+    axarr[0].set_title(r"$R_{00}$")
+    axarr[0].plot(Rs[:, 0, 0], label="weighted average")
+    axarr[0].plot(truth[:, 0, 0], label="truth")
+    axarr[0].plot(Rs_estimated[:, 0, 0], 'o', label="estimations")
+    axarr[0].set_ylabel(r"$\sigma_{\dot{\psi}}^2$ (deg/s)$^2$")
+    axarr[0].legend(loc="upper right")
 
-    axarr[1].set_title("R[1, 1]")
+    axarr[1].set_title(r"$R_{11}$")
+    axarr[1].set_ylabel(r"$\sigma_v^2$ (m/s)$^2$")
     axarr[1].plot(Rs[:, 1, 1])
-    axarr[1].plot([truth[1, 1]] * len(Rs))
+    axarr[1].plot(truth[:, 1, 1])
     axarr[1].plot(Rs_estimated[:, 1, 1], 'o')
+    axarr[1].set_xlabel("Window")
 
     plt.show()
 
@@ -213,6 +218,8 @@ def run_tracker():
     Rs = [tracker.R]
     Rs_estimated = [tracker.R]
     R_avg = tracker.R
+    sim_vars = [sim_var] * num_windows
+    truths = [R_proto * sim_vars[0]]
     for i in range(num_windows):
         print("Window", i + 1)
         readings, filtered, residuals, Ps, time, controls = filtering(
@@ -223,12 +230,11 @@ def run_tracker():
         update_coefficient = (1 - 0.9) / (1 - 0.9**(i + 1))
         R = R_avg * (1-update_coefficient) + \
             update_coefficient * R_estimated
-        # R = R_avg * average_coefficient + \
-        #     (1-average_coefficient) * R_estimated
         R_avg = R
         print(R)
         tracker.R = R
         Rs.append(R)
+        truths.append(R_proto * sim_vars[i])
 
         if len(Rs) == 2:
             total_readings = readings
@@ -241,9 +247,11 @@ def run_tracker():
 
     Rs = np.asarray(Rs)
     Rs_estimated = np.asarray(Rs_estimated)
+    truths = np.asarray(truths)
 
-    plot_noise_matrices_with_data(Rs, Rs_estimated, R_proto * sim_var,
-                                  total_filtered, total_readings)
+    # plot_noise_matrices_with_data(Rs, Rs_estimated, R_proto * sim_var,
+    #                               total_filtered, total_readings)
+    plot_noise_matrices(Rs, Rs_estimated, truths)
 
 
 if __name__ == "__main__":
